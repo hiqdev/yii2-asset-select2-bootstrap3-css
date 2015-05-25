cca91164db4e7cb1ac45ddc1c38d2cab6f62e381
SilverFire - Dima Naumenko <d.naumenko.a@gmail.com>
2015-05-25 14:42:59 +0000

    Added Select2Bootstrap3CssAsset

---
+++ Select2Bootstrap3CssAsset.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @link    http://hiqdev.com/yii2-asset-select2-bootstrap3-css
+ * @license http://hiqdev.com/yii2-asset-select2-bootstrap3-css/license
+ * @copyright Copyright (coffee) 2015 HiQDev
+ */
+
+namespace hiqdev\assets\select2_bootstrap3_css;
+
+/**
+ * Class Asset
+ *
+ * @package hipanel\yii2-asset-select2-bootstrap-css
+ */
+class Asset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@vendor/silverfire/select2-bootstrap3-css';
+
+    public $css = [
+        'select2-bootstrap.min.css'
+    ];
+}
