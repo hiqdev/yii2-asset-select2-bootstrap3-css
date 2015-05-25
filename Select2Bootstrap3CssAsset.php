6eb4b4dab3e0a49b1c8111991eb90a474c8e85af
Andrii Vasyliev <sol@hiqdev.com>
2015-05-25 18:20:46 +0000

    fixed class name

--- Select2Bootstrap3CssAsset.php
+++ Select2Bootstrap3CssAsset.php
@@ -12,7 +12,7 @@ namespace hiqdev\assets\select2_bootstrap3_css;
  *
  * @package hipanel\yii2-asset-select2-bootstrap-css
  */
-class Asset extends \yii\web\AssetBundle
+class Select2Bootstrap3CssAsset extends \yii\web\AssetBundle
 {
     public $sourcePath = '@vendor/silverfire/select2-bootstrap3-css';
 
